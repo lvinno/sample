ecc3e8773eecc91186e0a47dd3f5adae10091491
lvino <lyuniancheng@gmail.com>
2018-02-17 10:07:35 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,19 +20,20 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|max:50',
-            'email' => 'required|email|unique:users|max:255',
-            'password' => 'required|confirmed|min:6'
-        ]);
+     $this->validate($request, [
+         'name' => 'required|max:50',
+         'email' => 'required|email|unique:users|max:255',
+         'password' => 'required|confirmed|min:6'
+     ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
+     $user = User::create([
+         'name' => $request->name,
+         'email' => $request->email,
+         'password' => bcrypt($request->password),
+     ]);
 
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show', [$user]);
+     Auth::login($user);
+     session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+     return redirect()->route('users.show', [$user]);
     }
 }
