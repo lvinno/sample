79249e4f608adb4800fa20a32a59ff5c68194537
lvino <lyuniancheng@gmail.com>
2018-04-02 12:39:41 +0000

    Finish user edit, update, index and destroy actions.

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,27 +7,33 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     //
     public function create()
   {
       return view('sessions.create');
   }
 
-  public function store(Request $request)
-  {
-     $credentials = $this->validate($request, [
-         'email' => 'required|email|max:255',
-         'password' => 'required'
-     ]);
+    public function store(Request $request)
+     {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
 
-     if (Auth::attempt($credentials, $request->has('remember'))) {
-         session()->flash('success', '欢迎回来！');
-         return redirect()->route('users.show', [Auth::user()]);
-     } else {
-         session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-         return redirect()->back();
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->intended(route('users.show', [Auth::user()]));
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back();
+        }
      }
-  }
 
    public function destroy()
   {
@@ -35,4 +41,6 @@ class SessionsController extends Controller
       session()->flash('success', '您已成功退出！');
       return redirect('login');
   }
+
+
 }
