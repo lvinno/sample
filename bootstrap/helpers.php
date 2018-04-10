48121f4b0cffab0151e6b624ae1ddf2a42fbc56e
lvino <lyuniancheng@gmail.com>
2018-04-10 15:07:48 +0000

    adjust

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,10 +1,11 @@
 <?php
 
+
 function get_db_config()
 {
-    if (getenv("DATABASE_URL")) {
+    if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
-        //echo $url;
+
         return $db_config = [
             'connection' => 'pgsql',
             'host' => $url["host"],
@@ -13,7 +14,6 @@ function get_db_config()
             'password'  => $url["pass"],
         ];
     } else {
-
         return $db_config = [
             'connection' => env('DB_CONNECTION', 'mysql'),
             'host' => env('DB_HOST', 'localhost'),
